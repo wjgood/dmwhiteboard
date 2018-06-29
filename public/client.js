c49469fad4d972faf997003a22ff276fd3d122fb
Wesley Good <wjgood@ksu.edu>
2018-06-29 17:06:34 -0500

    Added NPC creation

--- public/client.js
+++ public/client.js
@@ -1,34 +1,44 @@
 var socket = io(); //we'll use this later
 
 var initList = [];
-var pcList = [];
+//var pcList = [];
+//var npcList = [];
 
 function addPC(name) {
   var newPc = {
+    type: "pc",
     name: name,
     init: 0
   };
   //console.log("Adding PC: ");
   //console.log(newPc);
-  pcList.push(newPc);
+  //pcList.push(newPc);
+  initList.push(newPc);
+}
+
+function addNPC(name, hp) {
+  var newNpc = {
+    type: "npc",
+    name: name,
+    hp: hp,
+    init: 0
+  };
+  initList.push(newNpc);
 }
 
 function findPCindex(name) {
-  return pcList.findIndex(obj => obj.name == name);
+  return initList.findIndex(obj => obj.name == name);
 }
 
 function editPCinit(name, init) {
-  pcList[findPCindex(name)].init = init;
+  initList[findPCindex(name)].init = init;
   updateInitList()
 }
 
 function deletePC(name) {
-  pcList.splice(findPCindex(name),1);
+  initList.splice(findPCindex(name),1);
 }
 
-function returnPCs() {
-  return pcList;
-}
 
 function initCompare(pcA, pcB) {
   return (pcB.init - pcA.init);
@@ -37,11 +47,11 @@ function initCompare(pcA, pcB) {
 function updateInitList() {
   //console.log("PC List: ");
   //console.log(pcList);
-  pcList.sort(initCompare);
+  initList.sort(initCompare);
   //console.log("PC List: ");
   //console.log(pcList);
   $('#init-list').empty();
-  for (var i = 0; i < pcList.length; i++)
+  for (var i = 0; i < initList.length; i++)
   {
     //console.log("PC: ");
     //console.log(pcList[i]);
@@ -56,48 +66,71 @@ function updateInitList() {
     var nameCol = $('<div>')
       .addClass('col')
       .addClass('no-border')
-      .append(pcList[i].name)
+      .append(initList[i].name)
       .appendTo(initPlayer);
     var controlCol = $('<div>')
-      .addClass('col-4')
+      .addClass('col-5')
       .addClass('no-border')
       .appendTo(initPlayer);
+    //
+    // var initBadge = $('<span>')
+    //   .addClass('badge')
+    //   .append(pcList[i].init)
+    //   .appendTo(initCol);
+    // if (pcList[i].init == 0) initBadge.addClass('badge-secondary');
+    // else if (pcList[i].init <= 5) initBadge.addClass('badge-danger');
+    // else if (pcList[i].init <= 10) initBadge.addClass('badge-warning');
+    // else if (pcList[i].init <= 15) initBadge.addClass('badge-info');
+    // else if (pcList[i].init <= 20) initBadge.addClass('badge-primary');
+    // else if (pcList[i].init > 20) initBadge.addClass('badge-success');
 
-    var initBadge = $('<span>')
-      .addClass('badge')
-      .append(pcList[i].init)
-      .appendTo(initCol);
-    if (pcList[i].init == 0) initBadge.addClass('badge-secondary');
-    else if (pcList[i].init <= 5) initBadge.addClass('badge-danger');
-    else if (pcList[i].init <= 10) initBadge.addClass('badge-warning');
-    else if (pcList[i].init <= 15) initBadge.addClass('badge-info');
-    else if (pcList[i].init <= 20) initBadge.addClass('badge-primary');
-    else if (pcList[i].init > 20) initBadge.addClass('badge-success');
-
-    var btnGroup = $('<div>')
+    var initBtnGroup = $('<div>')
       .addClass('btn-group')
-      .appendTo(controlCol);
+      .appendTo(initCol);
     var initButton = $('<button>')
       .attr('type', 'button')
       .addClass('btn')
-      .addClass('btn-primary')
+      //.addClass('btn-primary')
       .addClass('dropdown-toggle')
       .attr('data-toggle', 'dropdown')
-      .append('<i class="fas fa-info"></i>')
-      .appendTo(btnGroup);
+      .append(initList[i].init)
+      .appendTo(initBtnGroup);
+    if (initList[i].init == 0) initButton.addClass('btn-secondary');
+    else if (initList[i].init <= 5) initButton.addClass('btn-danger');
+    else if (initList[i].init <= 10) initButton.addClass('btn-warning');
+    else if (initList[i].init <= 15) initButton.addClass('btn-info');
+    else if (initList[i].init <= 20) initButton.addClass('btn-primary');
+    else if (initList[i].init > 20) initButton.addClass('btn-success');
+
     var initMenu = $('<div>')
       .addClass('dropdown-menu')
       .addClass('scrollable-menu')
-      .appendTo(btnGroup);
+      .appendTo(initBtnGroup);
     for (var ii = 30; ii > 0; ii--)
     {
       var num = $('<button>')
         .addClass('btn')
         .addClass('dropdown-item')
-        .attr('onclick','editPCinit("'+pcList[i].name+'", '+ii+')')
+        .attr('onclick','editPCinit("'+initList[i].name+'", '+ii+')')
         .append(ii)
         .appendTo(initMenu);
     }
+
+    var controllBtnGroup = $('<div>')
+      .addClass('btn-group')
+      .appendTo(controlCol);
+    var Button = $('<button>')
+      .attr('type', 'button')
+      .addClass('btn')
+      .append('Button')
+      .appendTo(controllBtnGroup);
+    if (initList[i].init == 0) Button.addClass('btn-secondary');
+    else if (initList[i].init <= 5) Button.addClass('btn-danger');
+    else if (initList[i].init <= 10) Button.addClass('btn-warning');
+    else if (initList[i].init <= 15) Button.addClass('btn-info');
+    else if (initList[i].init <= 20) Button.addClass('btn-primary');
+    else if (initList[i].init > 20) Button.addClass('btn-success');
+
 
     initPlayer.appendTo('#init-list');
   }
@@ -105,13 +138,53 @@ function updateInitList() {
 
 $('#addPC').keypress(function(event) {
   if (event.which == 13) {
-    if ($('#addPC'))
+    if ($('#addPC').val())
     {
       //console.log("Adding PC: " + $('#addPC').val());
       addPC($('#addPC').val());
       $('#addPC').val('');
       updateInitList();
-      $('[data-toggle="popover"]').popover();
     }
   }
 });
+
+$('#addNpcName').keypress(function(event) {
+  if (event.which == 13) {
+    var npcName = $('#addNpcName').val();
+    var npcHP = $('#addNpcHp').val();
+    tryAddNPC(npcName, npcHP);
+  }
+});
+
+$('#addNpcHp').keypress(function(event) {
+  if (event.which == 13) {
+    var npcName = $('#addNpcName').val();
+    var npcHP = $('#addNpcHp').val();
+    tryAddNPC(npcName, npcHP);
+  }
+});
+
+function tryAddNPC(name, hp) {
+  if (name && hp)
+  {
+    addNPC(name, hp);
+    $('#addNpcName').val('');
+    $('#addNpcHp').val('');
+    updateInitList();
+  }
+  else
+  {
+    var alert = $('<div>')
+      .addClass('row')
+      .addClass('alert')
+      .addClass('alert-warning')
+      .addClass('alert-dismissible')
+      .addClass('fade')
+      .addClass('show')
+      .attr('role', 'alert')
+      .append('Make sure your NPC has both a name and HP!')
+      .append('<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>')
+      .insertBefore($('#rowAddNPC'));
+
+  }
+}
