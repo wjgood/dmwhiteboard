9068fc3637b4f59ccdeb6963114709decf7232fc
Wesley Good <wjgood@ksu.edu>
2018-06-29 15:23:35 -0500

    Add edit PCs in Init list. list auto sorts.

--- public/client.js
+++ public/client.js
@@ -1,6 +1,6 @@
 var socket = io(); //we'll use this later
 
-var masterList = [];
+var initList = [];
 var pcList = [];
 
 function addPC(name) {
@@ -8,6 +8,8 @@ function addPC(name) {
     name: name,
     init: 0
   };
+  //console.log("Adding PC: ");
+  //console.log(newPc);
   pcList.push(newPc);
 }
 
@@ -17,6 +19,7 @@ function findPCindex(name) {
 
 function editPCinit(name, init) {
   pcList[findPCindex(name)].init = init;
+  updateInitList()
 }
 
 function deletePC(name) {
@@ -32,9 +35,83 @@ function initCompare(pcA, pcB) {
 }
 
 function updateInitList() {
+  //console.log("PC List: ");
+  //console.log(pcList);
   pcList.sort(initCompare);
-  for (var pc in pcList)
+  //console.log("PC List: ");
+  //console.log(pcList);
+  $('#init-list').empty();
+  for (var i = 0; i < pcList.length; i++)
   {
+    //console.log("PC: ");
+    //console.log(pcList[i]);
+    var initPlayer = $('<li>')
+      .addClass('list-group-item');
+    var initBadge = $('<span>')
+      .addClass('badge')
+      //.addClass('badge-secondary')
+      .addClass('mr-1')
+      .append(pcList[i].init);
+    if (pcList[i].init == 0) initBadge.addClass('badge-secondary');
+    else if (pcList[i].init <= 5) initBadge.addClass('badge-danger');
+    else if (pcList[i].init <= 10) initBadge.addClass('badge-warning');
+    else if (pcList[i].init <= 15) initBadge.addClass('badge-info');
+    else if (pcList[i].init <= 20) initBadge.addClass('badge-primary');
+    else if (pcList[i].init > 20) initBadge.addClass('badge-success');
+    // var initButton = $('<a>')
+    //   //.attr('type', 'button')
+    //   .attr('href', '#')
+    //   .addClass('initEdit')
+    //   .attr('id', pcList[i].name)
+    //   //.addClass('btn')
+    //   //.addClass('btn-sm')
+    //   //.addClass('btn-primary')
+    //   .addClass('ml-4')
+    //   .attr('data-toggle', 'popover')
+    //   .attr('data-trigger', 'click')
+    //   .attr('data-html', 'true')
+    //   //.attr('title', 'Set Initiative')
+    //   .attr('data-content', '<div class="input-group input-group-sm mb-3"><div class="input-group-prepend"><span class="input-group-text" id="inputGroup-sizing-sm">Set Initiative</span>  </div>  <input type="text" class="form-control" aria-label="Small" aria-describedby="inputGroup-sizing-sm"></div>')
+    //   .append('<i class="fas fa-info"></i>');
 
+    var btnGroup = $('<div>')
+      .addClass('btn-group')
+      .addClass('ml-4');
+    var initButton = $('<button>').attr('type', 'button')
+      .addClass('btn').addClass('btn-primary')
+      .addClass('dropdown-toggle')
+      .attr('data-toggle', 'dropdown')
+      .append('<i class="fas fa-info"></i>')
+      .appendTo(btnGroup);
+    var initMenu = $('<div>')
+      .addClass('dropdown-menu')
+      .addClass('scrollable-menu')
+      .appendTo(btnGroup);
+    for (var ii = 30; ii > 0; ii--)
+    {
+      var num = $('<button>')
+        .addClass('btn')
+        .addClass('dropdown-item')
+        .attr('onclick','editPCinit("'+pcList[i].name+'", '+ii+')')
+        .append(ii)
+        .appendTo(initMenu);
+    }
+    initPlayer.append(initBadge);
+    initPlayer.append(pcList[i].name);
+    initPlayer.append(btnGroup);
+    initPlayer.appendTo('#init-list');
   }
 }
+
+$('#addPC').keypress(function(event) {
+  if (event.which == 13) {
+    if ($('#addPC'))
+    {
+      //console.log("Adding PC: " + $('#addPC').val());
+      addPC($('#addPC').val());
+      $('#adPC').val('');
+      updateInitList();
+      $('[data-toggle="popover"]').popover();
+    }
+  }
+});
